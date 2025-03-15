87097d6f208e8b6ec132a664a45980f7662a0ba9
Bone-Structure <abbigreene13@gmail.com>
2025-03-15 11:10:22 -0400

    Moved context

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ function generatePoem(event){
 
    let instructionsInput = document.querySelector("#user-instructions") ;
    let apiKey="3e7751ao608a0543f0ba87d742et2457";
+   let context="You are a romantic Poem expert. Your mission is to generate a 8 line poem in basic HTML format and seperate each line with a <br/>. Make sure to follow the User insrtuctions. Sign code with 'SheCodes AI' inside a <strong> element at the bottom of the poem. Do not include `";
    let prompt=`User Instructions:Generate Chinese poem about ${instructionsInput.vaule}`;
-   let context="Your a romantic poem expert. Your mission is to generate a 8 line poem in basic HTML. Make sure to follow the User insrtuctions. Sign code with 'SheCodes AI' inside a <strong> element at the bottom of the poem";
    let apiUrl= `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
 
 console.log("generating poem");
